3cb8b1dec651ad40721a7c3bd70a99d21773b8da
Lauri Elias <lauri.elias@outlook.com>
2020-11-04 01:26:15 +0200

    Work on finding appellations

--- check_if_followups.py
+++ check_if_followups.py
@@ -10,15 +10,30 @@ def ping_single_verdict(verdict_id: int):
     url = f'https://www.riigiteataja.ee/kohtulahendid/detailid.html?id={verdict_id}'
 
     response = requests.get(url)
+    if 'Apellatsioonmenetlus' in response.text:
+        return url
     if 'Seotud lahendid' in response.text:
         soup = bs4.BeautifulSoup(response.text, 'html.parser')
         case_number_a = soup.find_all('a', href=re.compile('/kohtulahendid/fail.html'))[0]
-        inner_id = int(case_number_a.contents[0].split('/')[1])
+        case_number_parts = case_number_a.contents[0].split('/')
+        main_case_number = case_number_parts[0]
+        current_case_number_subcase = int(case_number_parts[1])
         related_case_number_as = soup.find_all('a', href=re.compile('detailid.html\\?id'))
+        max_related_subcase_id = 0
+        max_subcase_link = None
         for each in related_case_number_as:
-            related_inner_id = int(each.contents[0].split('/')[1])
-            if related_inner_id > inner_id:
-                print(f'Verdict {verdict_id} has follow-up verdicts')
+            a_contents = each.contents[0]
+            a_contents = a_contents.replace('\\', '/')
+            if a_contents.startswith(main_case_number):
+                related_subcase_number = int(a_contents.split('/')[1])
+                if related_subcase_number > max_related_subcase_id and related_subcase_number > current_case_number_subcase:
+                    max_related_subcase_id = related_subcase_number
+                    max_subcase_link = f'https://www.riigiteataja.ee/kohtulahendid/{each.attrs["href"]}'
+
+        if max_subcase_link:
+            subresponse = requests.get(max_subcase_link)
+            if 'Apellatsioonmenetlus' in subresponse.text:
+                return max_subcase_link
 
 
 def run_over_all_files():
@@ -27,5 +42,16 @@ def run_over_all_files():
         responses = [executor.submit(ping_single_verdict, verdict_id) for verdict_id in ids]
         concurrent.futures.wait(responses)
 
-# for response in responses:
-#     print(response.result())
+    with open('with_followups.txt', 'w') as opened_file:
+        for response in responses:
+            result = response.result()
+            if result is not None:
+                opened_file.write(result + '\n')
+
+
+def main():
+    run_over_all_files()
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_all.py
+++ tests/test_all.py
@@ -5,10 +5,15 @@ import responses
 
 from check_if_followups import ping_single_verdict
 
-test_html = io.open(os.path.dirname(os.path.realpath(__file__)) + '/fixtures/240199396.html', encoding='utf-8').read()
+test_html = io.open(os.path.dirname(os.path.realpath(__file__)) + '/fixtures/240778738.html', encoding='utf-8').read()
+test_html_2 = io.open(os.path.dirname(os.path.realpath(__file__)) + '/fixtures/247765253.html', encoding='utf-8').read()
 
 
 @responses.activate
 def test_ping_single_verdict():
-    responses.add(responses.GET, 'https://www.riigiteataja.ee/kohtulahendid/detailid.html?id=240199396', body=test_html)
-    ping_single_verdict(240199396)
+    responses.add(responses.GET, 'https://www.riigiteataja.ee/kohtulahendid/detailid.html?id=240778738', body=test_html)
+    responses.add(responses.GET, 'https://www.riigiteataja.ee/kohtulahendid/detailid.html?id=247765253',
+                  body=test_html_2)
+    end_url = ping_single_verdict(240778738)
+
+    assert end_url == 'https://www.riigiteataja.ee/kohtulahendid/detailid.html?id=247765253'
